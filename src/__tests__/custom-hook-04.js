38fb09884fbb0266442f074a77e1497a179d2f95
joshuababcock15 <joshuababcock15@gmail.com>
2022-01-11 14:03:06 -0500

    react hooks

--- src/__tests__/custom-hook-04.js
+++ src/__tests__/custom-hook-04.js
@@ -1,6 +1,7 @@
 import {renderHook, act} from '@testing-library/react-hooks'
 import {useCounter} from '../use-counter'
 
+// love this
 test('exposes the count and increment/decrement functions', () => {
   const {result} = renderHook(useCounter)
   expect(result.current.count).toBe(0)
